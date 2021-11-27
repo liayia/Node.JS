250e58ce918a64b137e8c2d5592d4295a2596bf6
liayia <liayia.tw@gmail.com>
2021-11-27 13:29:28 +0800

    mail

---
+++ 2021_1127/hello.js
@@ -0,0 +1,17 @@
+//console.log("Hello World !!!");
+
+// Function (函式)
+// 兩數相加的函式
+// 左邊放要算的 , 右邊放算式
+let add = (a,b) => a+b;
+// console.log --> 將結果顯示到畫面上
+console.log( add(1,2) );
+console.log( add(-1,17) );
+
+let sayHello = (name) => {
+    return "Hello" + name + "!";
+};
+
+console.log( sayHello("Arthur1") );
+console.log( sayHello("Arthur2") );
+console.log( sayHello("Arthur3") );
---
+++ 2021_1127/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app     = express();
+
+app.get("/",function(req,res){
+    res.send("HIHI , 我是 Node.js Server !!!");
+});
+
+app.get("/testqq",function(req,res){
+    res.send("HIHI , 我是 /testqq 路徑");
+});
+
+app.listen(8088,function(){
+    console.log("Server is running at http://localhost:" + String(8088));
+});
+
+// 程式執行後 , 進入 "等待" (Pending)
