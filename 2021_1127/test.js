fd63d33128ffc83d14646d7934fedcd780a3cfc8
liayia <liayia.tw@gmail.com>
2021-11-27 16:44:48 +0800

    Update

--- 2021_1127/test.js
+++ 2021_1127/test.js
@@ -1,8 +1,11 @@
-Console.log( "a,b,c,d".split("c") );
+//console.log( "a,b,c,d".split("c") )
+//[ 'a,b,', ',d' ]
 
 //new Array(10).fill(0).map((v,i)=>-i).reduce((a,b)=>a+b);
+//-45
 
 //let main = ()=>{ let a = -123; if(a>0){ var b = 456; }else{ var b = 789; const c = 0; }; console.log(a,b,c); } main(); 
+//SyntaxError: Unexpected identifier
 
 //let func1 = async () => 12; let func2 = () => 25; let main = async ()=>{ func1() .then(async r1=>{ let r2 = await func2(); console.log(r1,r2); }) .catch(err=>{ console.log("Error!"); }) }; main(); 
 //12 25
---
+++ 2021_1127/tutorial.js
@@ -0,0 +1,15 @@
+//console.log("HIHI , I'm Arthur");
+
+// 資料型別
+// Number , 數值資料
+let a = 123 // 宣告變數
+let b = 456 // 宣告變數
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+// String , 字串 , 文字資料
+let name2 = "Arthur";
+let message2 = "要下課了啦啦啦~";
+console.log(name2,message2);
