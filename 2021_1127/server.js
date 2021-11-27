a099cd499eedf7262d31f3daf15e9bbdd48fd782
liayia <liayia.tw@gmail.com>
2021-11-27 13:47:50 +0800

    20211127_2

--- 2021_1127/server.js
+++ 2021_1127/server.js
@@ -13,6 +13,15 @@ app.get("/this-is-a-book",(req,res) => {
     res.send("This is a book ~~~");
 });
 
+app.get("/data",(req,res) => {
+    let name = req.query.name;
+    let age  = req.query.age;
+    let message = req.query.message;
+    console.log(name,age,message);
+    //res.send("HIHI , " + name + " 您好~");
+    res.send("HIHI , 您回傳的參數為: " + name + "/" + age + "/" + message );
+});
+
 app.listen(8088,function(){
     console.log("Server is running at http://localhost:" + String(8088));
 });
