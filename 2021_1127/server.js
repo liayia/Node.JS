460e45f9e9dc1682ed98183374d59e4ace809163
liayia <liayia.tw@gmail.com>
2021-11-27 13:30:02 +0800

    20211127

--- 2021_1127/server.js
+++ 2021_1127/server.js
@@ -9,6 +9,10 @@ app.get("/testqq",function(req,res){
     res.send("HIHI , 我是 /testqq 路徑");
 });
 
+app.get("/this-is-a-book",(req,res) => {
+    res.send("This is a book ~~~");
+});
+
 app.listen(8088,function(){
     console.log("Server is running at http://localhost:" + String(8088));
 });
