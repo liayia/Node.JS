303cc07725ab8a32b5d41696da096f3f32fee129
liayia <liayia.tw@gmail.com>
2021-12-18 10:14:33 +0800

    Uploading code

--- 2021_1218/server.js
+++ 2021_1218/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
 const app = express();
 const portNum = 8088;
 
+// 設定模板引擎
+app.engine('html',hbs.__express);
+// 設定模板 (template) 位置
+app.set("views" , path.join(__dirname ,"application","views"));
+// 設定靜態檔 位置
+app.use(express.static(path.join(__dirname,"application")));
+
 app.get("/" , (req,res)=>{
-  res.send("嗨嗨,  我是 Node.js server.");
+  // res.send("嗨嗨,  我是 Node.js server.");
+  res.render("index.html");
 });
 
 app.listen(portNum , ()=>{
