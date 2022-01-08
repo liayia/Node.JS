153495211d928102f4a63dedf903347dc597ab88
liayia <liayia.tw@gmail.com>
2022-01-08 18:54:59 +0800

    update

--- 2022_0108/mongo-tutorial1.js
+++ 2022_0108/mongo-tutorial1.js
@@ -1,23 +1,24 @@
-//// 初步教學
-
+////// 初步教學
+//// 新增 (insert)
 
 // .insertMany 
 // 將 sample-3-models/sample-data/sample2.json 塞入
 db.getCollection('mongo-tutorial').insertMany([{
-    "dramaId" : "1001",
-    "category" : "犯罪",
-    "name" : "絕命毒師",
-    "score" : 10.0
-}, ... ])
+  "dramaId" : "1001",
+  "category" : "犯罪",
+  "name" : "絕命毒師",
+  "score" : 10.0
+}, ... ]);
 // 總共有 7 筆
-
-////////////////////////////////////////////////////////////
 
 // .insert 新增一筆資料
 db.getCollection('mongo-tutorial').insert( { "today" : "星期六" , "weather" : "sunny"} )
 
 // .find 查看 全部資料
 db.getCollection('mongo-tutorial').find({})
+
+////////////////////////////////////////////////////////////
+//// 查詢 (find)
 
 // 有條件的 .find(condition)
 // ex1: category="政治"
@@ -28,39 +29,39 @@ db.getCollection('mongo-tutorial').find({ "category" : "政治" , "score" : 9.5 
 
 // ex3: category="政治" && score > 9.5
 db.getCollection('mongo-tutorial').find({  
-  "category" : "政治" ,
-  "score" : {
-    "$gt" : 9.5      // > 9.5
-    // "$gte" : 9.5  // >= 9.5 
-  }
+"category" : "政治" ,
+"score" : {
+  "$gt" : 9.5      // > 9.5
+  // "$gte" : 9.5  // >= 9.5 
+}
 })
 
 // ex4: score <= 9.5
 db.getCollection('mongo-tutorial').find({  
-    "score" : {  
-         //"$lt" : 9.5  
-         "$lte" : 9.5   
-    }    
+  "score" : {  
+       //"$lt" : 9.5  
+       "$lte" : 9.5   
+  }    
 })
 
 // ex5: 找到 age 欄位存在的資料
 db.getCollection('mongo-tutorial').find({  
-    "age" : { 
-        "$exists" :  true
-     }
+  "age" : { 
+      "$exists" :  true
+   }
 })
 
 // ex6: today 欄位'不存在' && category=政治 && score >=9.4 
 db.getCollection('mongo-tutorial').find({  
-     "today" : { 
-        "$exists" :  false
-     },
-     "category" : "政治",
-     "score" : {
-        "$gte" : 9.4    
-     }
+   "today" : { 
+      "$exists" :  false
+   },
+   "category" : "政治",
+   "score" : {
+      "$gte" : 9.4    
+   }
 })
-  
+
 // ex7: 搜尋全部 + 顯示 {category , name} 欄位 , _id 不顯示
 // ==>  .find(條件 , 欄位顯示)
 db.getCollection('mongo-tutorial').find( {} ,  { "category" : 1 , "name" : 1 , "_id" : 0 }  )
@@ -76,3 +77,27 @@ db.getCollection('mongo-tutorial').find( {} ,  { "category" : 1 , "name" : 1 , "
 //     "name" : "屍戰朝鮮",
 //     "score" : 9.0
 // }
+////////////////////////////////////////////////////////////
+//// 更新 (update)
+// ex1: 更新 _id=1234567 的資料 , 欄位為 score=8.1 , category=其他_QQQQ
+db.getCollection('mongo-tutorial').updateOne(
+  { "_id"  : "1234567" } , 
+  { "$set" : {
+       "score" :8.1,
+       "category" : "其他_QQQQ"  
+    } 
+  } 
+)
+
+// ex2: 更新 category=政治 多組資料 , 欄位為 age=100 , math_score=123 , message=卡卡的
+db.getCollection('mongo-tutorial').updateMany(
+  { "category"  : "政治" } , 
+  { "$set" : {
+       "age" : 100,
+       "math_score" : 123,
+       "message" : "卡卡的~"
+    } 
+  } 
+)
+
+// 有欄位 -> update value 值 ; 沒有欄位 -> 新增 key-value pair 
