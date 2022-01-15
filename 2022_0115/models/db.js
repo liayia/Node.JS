d421005bbac77198be7f6efc11e280b643d1151a
liayia <liayia.tw@gmail.com>
2022-01-15 13:08:11 +0800

    update

--- 2022_0115/models/db.js
+++ 2022_0115/models/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 //// 1. 建立連線 (connection)
 // 連線的設定值: "mongodb://{host_ip}:{port}/{db_name}"
-const connConfig = "mongodb://localhost:27017/nodejs-tutorial";
+const connConfig = "mongodb://localhost:27017/Nodejs";
 const conn = mongoose.createConnection(connConfig);
 
 // 一旦連線成功 , 觸發 callback function 
